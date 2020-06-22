d147c7cba0ff88bde6139dfe914204db9ff3ebde
mohamilr <ibrahimdamy7@gmail.com>
2020-06-23 00:21:36 +0100

    env

--- src/config/serviceAccount.config.ts
+++ src/config/serviceAccount.config.ts
@@ -1,12 +1,15 @@
+require ('dotenv').config();
+const env = process.env;
+
 module.exports = {
-  type: 'service_account',
-  project_id: 'covid-19-dd561',
-  private_key_id: 'edbc9b40e1608de16ceafd2be901fd9fd9822084',
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDCgBsX5IILDGQF\nR6lmI9qYQ4XF6qOA6holjXGtCAMN+I5qbVzC5iZyEVWozNmriGQj05LXXPiDYISo\n1tpO3imKi5NgvzjsbjOCPESC5lRi/XwG5NeMh/sREpGk1AmSg6b7yeR4oT4sS7t8\n6cz75xjdS7A30ig1do6mqXilqJVCDl3phXaXbqO2MKEyYDBXOn6mpHNUThn2dIjK\nDv6YlanIyST1SX48g47fItpw57N2u9s8ge0w23uBGzgxVx2dLa3VOG00oIxdjMoI\nimTq1R5CASmuBV0tymDRqicaAXsAyV5QsOuJHx45Tk1eYsrfrK6cAqJvTmvyn+f3\nBPLrXT2tAgMBAAECggEAGc/5iIK1dXToA0Fe7GS+um1foAQDPfUTkj9CwlVpXl3J\nvLsVz/HOUV4WkafaDWc07wJBnx5CBuF0yZl18HiQZ8gdP34xNkNrx5FrADpEIb0F\nTD7rkkVrnvjpZkgTmyOosgq5rP6tKyDD2ZCHuTiStq+4S7gyyOGajei4z7dfU5wr\n8cZ+xk6r7qRwJVMfmltw0gCgVlAtAH8WGqGx0jJuU8QBZF3pvFaneLZUjKcIA9wG\n07h8zsmaWIPqMP4KD1eJOz8EsoubVUUokQcQ36LeimXb//PnfyflfimPS0H4d09G\nNQVxLricIQ+r+p1fi7KXqZ72yiexuRprRY6Dc6P5sQKBgQD3zf+8HM34YkZMi3Dp\nD9fwyHJfnpMd3fvCEnkamy1kdPcOVM2KBASucgh26zjTm1XVmYPOcxGH9xxqgCtu\nFagktxWwHF4OPHcuhefU8yDKdyVgYd23gqutljm5qUAhGXVjny90Vj3c6mSdu0zA\nCnDwsb6SSyPmvU/+EZ/XCzzTRQKBgQDI7tB1kNJ+WAKtub/oLdILQeOnzrvnEygJ\nWUVgP2PtuhBOgre7HKAGT7Rg36jhNtVONojmPDWdtO52MDMszYqrbd1G0/5yCRxh\nzMoYRPI04Q31irvcvLl1WJO4R/ePwlZkcKxrDXJACZ3v6XOqlR9i9WNaj51NAq6T\nhgGqrvlzSQKBgQDDKeOSpOD3EXcqS9v2Qjsg8M1bT4mwSSdBlsj4UmSyrIfzKoji\nfP2u4jKWbfVIextj6c7sJqYU/mLL4fvSbI4LtIIJPJ46+hYnduC6T21w59txCELV\nh71sLldySMEwEFvqxPFb35xDQ/tQWrY8vTxr5KEhmLpCvLCmefz4IWqbPQKBgBv6\n1WBpqEYbr1PNt+F27/bNWAw4B4Zn7n1OxxBeA2ZJMJgeL8OIeugmFTIgMv4Zip53\njZyx6a1qUoJ2t8mXXaX9iICiaJ4skp0zzTiP8UJXzH/Sr9JPHWwvx3Iz409LeohG\nAFxv5Obx7+JGmeq5FUPMc+dadEcTBohYcsb30pixAoGAB37rqoq61zGpsGoqIJwR\nCAhDVmgmRsv7zGUbghDw+zmnOMzqHXpaYYUwKfwROgL3Jm0Cvq2Y2RiEwKg57cuh\nq432RghH6Kr8WABvuKBFjAYVjoaAGlmNLoRXoj2vuLKnKQ6/kCeHR84Tmx8kRvd/\nCtZjw3i/esMkAL0v1cjO3tk=\n-----END PRIVATE KEY-----\n",
-  client_email: 'firebase-adminsdk-u0t3z@covid-19-dd561.iam.gserviceaccount.com',
-  client_id: '114313555514385049449',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-u0t3z%40covid-19-dd561.iam.gserviceaccount.com'
+  type: env.TYPE,
+  project_id: env.PROJECT_ID,
+  private_key_id: env.PRIVATE_KEY_ID,
+  private_key: env.PRIVATE_KEY,
+  client_email: env.CLIENT_EMAIL,
+  client_id: env.CLIENT_ID,
+  auth_uri: env.AUTH_URI,
+  token_uri: env.TOKEN_URI,
+  auth_provider_x509_cert_url: env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: env.CLIENT_X509_CERT_URL
 }
